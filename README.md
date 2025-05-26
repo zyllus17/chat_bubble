fb5bcf6d08cc3a17d95ed4a2b92c4ff12de80969
Maruf <mrfhassan17@gmail.com>
2025-05-26 23:41:44 +0530

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# chat_bubble
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/chat_bubble/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chat_bubble
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
